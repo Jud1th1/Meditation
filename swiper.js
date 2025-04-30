e9696607906411b99060293ec7d965b36e854059
Jud1th1 <127470572+Jud1th1@users.noreply.github.com>
2025-04-29 23:51:55 -0400

    Index page now updated

--- swiper.js
+++ swiper.js
@@ -1,23 +1,21 @@
-var swiper = new Swiper('.swiper', {
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper('.swiper', {
     slidesPerView: 2,
-    spaceBetween: 10,
-    // Optional parameters
-    direction: 'horizontal',
+    spaceBetween: 15,
     loop: true,
-  
-    // If we need pagination
+    grabCursor: true,
     pagination: {
       el: '.swiper-pagination',
+      clickable: true
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-prev'
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    breakpoints: {
+      768: { slidesPerView: 3 },
+      480: { slidesPerView: 2 },
+      0:   { slidesPerView: 1 }
+    }
   });
+});
